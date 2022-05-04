a4d74cdf24d40f69ee4be35537cbdb16a7f8e273
aurbalejo <aurbalejo@uts.edu.mx>
2022-05-03 22:20:42 -0700

    Update 3botones2led1DHT11.ino

--- 3botones2led1DHT11/3botones2led1DHT11.ino
+++ 3botones2led1DHT11/3botones2led1DHT11.ino
@@ -30,6 +30,20 @@
 
 // Bibliotecas
   #include "DHT.h"
+#include <WiFi.h>  // Biblioteca para el control de WiFi
+#include <PubSubClient.h> //Biblioteca para conexion MQTT
+
+const char* ssid = "MEGACABLE-279F";  // Aquí debes poner el nombre de tu red
+const char* password = "Notiene13";  // Aquí debes poner la contraseña de tu red
+
+//Datos del broker MQTT
+const char* mqtt_server = "192.168.100.85"; // Si estas en una red local, coloca la IP asignada, en caso contrario, coloca la IP publica
+IPAddress server(192,168,100,85);
+
+// Objetos
+WiFiClient espClient; // Este objeto maneja los datos de conexion WiFi
+PubSubClient client(espClient); // Este objeto maneja los datos de conexion al broker
+
 
 // Constantes
 const int pinBOTON1 = 13; // Manual. Activa la refrigeración al ser presionado
@@ -42,8 +56,13 @@ const double temAlta = 32.5;  // set point de temperatura Alta
 // Variables
 bool EstadoBoton1;              // Estado lógico BOTON1
 bool EstadoBoton2, EstadoBoton3;      // Estado lógico BOTON2 Y BOTON3
+bool BtnWeb1;
 double TiempoActual, TiempoObjetivo;
 float t=0;
+long timeNow, timeLast; // Variables de control de tiempo no bloqueante
+int data = 0; // Contador
+int wait = 5000;  // Indica la espera cada 5 segundos para envío de mensajes MQTT
+
 
 // Definición de objetos
   #define DHTPIN 12    // Pin 12 se conecta el DHT11
@@ -53,7 +72,9 @@ float t=0;
 // Condiciones iniciales - Se ejecuta sólo una vez al energizar
 void setup() {// Inicio de void setup ()
   Serial.begin (115200);  // Se inicia comunicación serial a 115200 baudios
-  Serial.println("Conectado... ");
+  Serial.print("Conectar a ");
+  Serial.println(ssid);
+  
   dht.begin();
   pinMode (pinBOTON1, INPUT_PULLUP); // Configurar el pin 13 BOTON1 como entrada con lógica inversa (PULLUP)
   pinMode (pinBOTON2, INPUT_PULLUP); // Configurar el pin 14 BOTON1 como entrada con lógica inversa (PULLUP)
@@ -63,6 +84,35 @@ void setup() {// Inicio de void setup ()
   digitalWrite (LED1, LOW);
   digitalWrite (LED2, LOW);
   TiempoObjetivo = millis() + 2000;  // Se asigna al setpoint el tiempo actual en milisegundos y se le suman 2000 milisegundos
+
+  WiFi.begin(ssid, password); // Esta es la función que realiz la conexión a WiFi
+ 
+  while (WiFi.status() != WL_CONNECTED) { // Este bucle espera a que se realice la conexión
+    //digitalWrite (statusLedPin, HIGH);
+    //delay(500); //dado que es de suma importancia esperar a la conexión, debe usarse espera bloqueante
+    //digitalWrite (statusLedPin, LOW);
+    Serial.print(".");  // Indicador de progreso
+    delay (500);
+  }
+  // Cuando se haya logrado la conexión, el programa avanzará, por lo tanto, puede informarse lo siguiente
+  Serial.println();
+  Serial.println("WiFi conectado");
+  Serial.println("Direccion IP: ");
+  Serial.println(WiFi.localIP());
+
+  // Si se logro la conexión, encender led
+  if (WiFi.status () > 0){
+  digitalWrite (LED2, LOW);
+  }
+  
+  delay (1000); // Esta espera es solo una formalidad antes de iniciar la comunicación con el broker
+
+  // Conexión con el broker MQTT
+  client.setServer(server, 1883); // Conectarse a la IP del broker en el puerto indicado
+  client.setCallback(callback); // Activar función de CallBack, permite recibir mensajes MQTT y ejecutar funciones a partir de ellos
+  delay(1500);  // Esta espera es preventiva, espera a la conexión para no perder información
+
+  timeLast = millis (); // Inicia el control de tiempo
   
 }// Fin de void setup
 
@@ -77,7 +127,26 @@ void loop() {// Inicio de void loop
 
   lecturaBotones();
   Logica();
+//********************************************
+   //Verificar siempre que haya conexión al broker
+  if (!client.connected()) {
+    reconnect();  // En caso de que no haya conexión, ejecutar la función de reconexión, definida despues del void setup ()
+  }// fin del if (!client.connected())
+  client.loop(); // Esta función es muy importante, ejecuta de manera no bloqueante las funciones necesarias para la comunicación con el broker
   
+  timeNow = millis(); // Control de tiempo para esperas no bloqueantes
+  if (timeNow - timeLast > wait) { // Manda un mensaje por MQTT cada cinco segundos
+    timeLast = timeNow; // Actualización de seguimiento de tiempo
+
+    //data++; // Incremento a la variable para ser enviado por MQTT
+    char dataString[8]; // Define una arreglo de caracteres para enviarlos por MQTT, especifica la longitud del mensaje en 8 caracteres
+    dtostrf(t, 2, 1, dataString);  // Esta es una función nativa de leguaje AVR que convierte un arreglo de caracteres en una variable String
+    Serial.print("Contador: "); // Se imprime en monitor solo para poder visualizar que el evento sucede
+    Serial.println(dataString);
+    client.publish("esp32/data", dataString); // Esta es la función que envía los datos por MQTT, especifica el tema y el valor
+  }// fin del if (timeNow - timeLast > wait) 
+
+//*************************************************
 }// Fin de void loop
 
 // Funcioes del usuario
@@ -119,17 +188,75 @@ void Logica(){
     digitalWrite (LED1, 0);
     }
   
-  //digitalWrite (LED2, !dato2);
   // Cuando debe de encender Refrigeración automática
-  // t > 28.5  con temperatura alta
+  // t > temAlta con temperatura alta
   // o 
   // ((!dato2 || !dato3) && dato)
   // Si se tiene alta demanda o sobrecarga y que no este presionado BOTON1 
   // de manual por la prioridad
-  if (t > temAlta || ((EstadoBoton2 || EstadoBoton3) && !EstadoBoton1)) {
+  if (BtnWeb1 || t > temAlta || ((EstadoBoton2 || EstadoBoton3) && !EstadoBoton1)) {
     digitalWrite (LED2, 1);
     }
   else{
     digitalWrite (LED2, 0);
     }
 }
+
+
+// Esta función permite tomar acciones en caso de que se reciba un mensaje correspondiente a un tema al cual se hará una suscripción
+void callback(char* topic, byte* message, unsigned int length) {
+
+  // Indicar por serial que llegó un mensaje
+  Serial.print("Llegó un mensaje en el tema: ");
+  Serial.print(topic);
+
+  // Concatenar los mensajes recibidos para conformarlos como una varialbe String
+  String messageTemp; // Se declara la variable en la cual se generará el mensaje completo  
+  for (int i = 0; i < length; i++) {  // Se imprime y concatena el mensaje
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+
+  // Se comprueba que el mensaje se haya concatenado correctamente
+  Serial.println();
+  Serial.print ("Mensaje concatenado en una sola variable: ");
+  Serial.println (messageTemp);
+
+  // En esta parte puedes agregar las funciones que requieras para actuar segun lo necesites al recibir un mensaje MQTT
+
+  // Ejemplo, en caso de recibir el mensaje true - false, se cambiará el estado del led soldado en la placa.
+  // El ESP323CAM está suscrito al tema esp/output
+  if (String(topic) == "esp32/output") {  // En caso de recibirse mensaje en el tema esp32/output
+    if(messageTemp == "true"){
+      Serial.println("Led encendido");
+      BtnWeb1=1;
+      //digitalWrite(flashLedPin, HIGH);
+    }// fin del if (String(topic) == "esp32/output")
+    else if(messageTemp == "false"){
+      Serial.println("Led apagado");
+      BtnWeb1=0;
+     // digitalWrite(flashLedPin, LOW);
+    }// fin del else if(messageTemp == "false")
+  }// fin del if (String(topic) == "esp32/output")
+}// fin del void callback
+
+// Función para reconectarse
+void reconnect() {
+  // Bucle hasta lograr conexión
+  while (!client.connected()) { // Pregunta si hay conexión
+    Serial.print("Tratando de contectarse...");
+    // Intentar reconexión
+    if (client.connect("ESP32CAMClient")) { //Pregunta por el resultado del intento de conexión
+      Serial.println("Conectado");
+      client.subscribe("esp32/output"); // Esta función realiza la suscripción al tema
+    }// fin del  if (client.connect("ESP32CAMClient"))
+    else {  //en caso de que la conexión no se logre
+      Serial.print("Conexion fallida, Error rc=");
+      Serial.print(client.state()); // Muestra el codigo de error
+      Serial.println(" Volviendo a intentar en 5 segundos");
+      // Espera de 5 segundos bloqueante
+      delay(5000);
+      Serial.println (client.connected ()); // Muestra estatus de conexión
+    }// fin del else
+  }// fin del bucle while (!client.connected())
+}// fin de void reconnect(
